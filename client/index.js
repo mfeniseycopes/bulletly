5c579b717ffec35684edd3f7e4d22ff88d216d2e
Meagan Fenisey-Copes <mfeniseycopes@gmail.com>
2017-08-20 17:16:42 -0400

    Initialize React in client

--- client/index.js
+++ client/index.js
@@ -1,1 +1,6 @@
-export default {}
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const renderReact = () => ReactDOM.render(<div>React is here</div>,  document.getElementById('react-root'))
+
+document.addEventListener('DOMContentLoaded', renderReact)
