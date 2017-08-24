de35a59c596660621f7355eea42e1c1c67cf698b
Meagan Fenisey-Copes <mfeniseycopes@gmail.com>
2017-08-24 13:36:46 -0400

    Connect Topics

--- client/components/Topics.js
+++ client/components/Topics.js
@@ -1,13 +1,39 @@
 import React from 'react'
+import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
+import { values } from 'ramda'
+
+import {
+  retrieveTopics,
+  createTopic,
+  updateTopic,
+  destroyTopic,
+} from '../actions'
 
 const topicLi = topic => (
   <li key={topic.id}>{topic.title}</li>
 )
 
-const Topics = ({ topics }) => (
-  <ul>
-    { topics.map(topicLi) }
-  </ul>
+const Topics = ({ topics, retrieveTopics }) => (
+  <section>
+    <ul>
+      { topics.map(topicLi) }
+    </ul>
+    <button onClick={retrieveTopics} >
+      Fetch Topics
+    </button>
+  </section>
 )
 
-export default Topics
+const mapStateToProps = (state, ownProps) => ({
+  topics: values(state.entities.topics)
+})
+
+const mapDispatchToProps = {
+  retrieveTopics,
+  createTopic,
+  updateTopic,
+  destroyTopic,
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Topics)
