5864526e3d48483201a047953466d7d0cc12221d
Meagan Fenisey-Copes <mfeniseycopes@gmail.com>
2017-08-28 17:49:07 -0400

    Use webpack for styling

--- client/components/App.js
+++ client/components/App.js
@@ -1,7 +1,9 @@
 import React from 'react'
 
+import styles from '../styles/app.css'
+
 const App = ({ children }) => (
-  <main>
+  <main className='app'>
     <h1>Bullet Journal</h1>
     { children }
   </main>
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,26 +1,31 @@
 var path = require('path');
 
 module.exports = {
-  context: __dirname,
-  entry: './index.js',
-  output: {
-    path: path.resolve(__dirname),
-    filename: '../public/bundle.js',
-  },
-  module: {
-    loaders: [
-      {
-        test: [/\.jsx?$/, /\.js?$/],
-        exclude: /(node_modules)/,
-        loader: 'babel-loader',
-        query: {
-          presets: ['es2015', 'react', 'stage-2'],
-        },
-      }
-    ]
-  },
-  devtool: 'source-map',
-  resolve: {
-    extensions: [".js", ".jsx", "*"],
-  },
+	context: __dirname,
+	entry: './index.js',
+	output: {
+		path: path.resolve(__dirname),
+		filename: '../public/bundle.js',
+	},
+	module: {
+		loaders: [
+			{
+				test: [/\.jsx?$/, /\.js?$/],
+				exclude: /(node_modules)/,
+				loader: 'babel-loader',
+				query: {
+					presets: ['es2015', 'react', 'stage-2'],
+				},
+			},
+			{
+				test: /\.css/,
+				loaders: ['style-loader', 'css-loader'],
+				include: __dirname + '/styles'
+			}
+		]
+	},
+	devtool: 'source-map',
+	resolve: {
+		extensions: [".js", ".jsx", "*"],
+	},
 };
