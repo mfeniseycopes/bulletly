1740262ca100aa93524ac043882e114abd8837cf
Meagan Fenisey-Copes <mfeniseycopes@gmail.com>
2017-09-15 12:15:51 -0400

    Fix silly imports

--- app/client/reducers/index.js
+++ app/client/reducers/index.js
@@ -20,4 +20,4 @@ if (process.env.NODE_ENV === 'development') {
   middlewares.push(require('redux-logger').default)
 }
 
-export default createStore(root, applyMiddleware(middlewares))
+export default createStore(root, applyMiddleware(...middlewares))
--- webpack.config.js
+++ webpack.config.js
@@ -94,7 +94,7 @@ module.exports = {
 		contentBase: path.resolve(__dirname, 'app', 'client'),
 		publicPath: 'http://localhost:8080/',
 		headers: {
-			"Access-Control-Allow-Origin": "http://localhost:3000",
+			"Access-Control-Allow-Origin": "http://localhost:5000",
 			"Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, PATCH, OPTIONS",
 			"Access-Control-Allow-Headers": "X-Requested-With, content-type, Authorization",
 			"Access-Control-Allow-Credentials": "true",
