971252e91cef0b417175b7d01b42a7a700a2a3f7
Meagan Fenisey-Copes <mfeniseycopes@gmail.com>
2017-11-16 17:13:15 -0500

    Add user to window for bootstrapping

--- app/server/config/passport.js
+++ app/server/config/passport.js
@@ -44,5 +44,13 @@ module.exports = app => {
             done(null, false, { message: 'Invalid credentials' }))
   ))
 
+  passport.isLoggedIn = (req, res, next) => {
+    if (req.isAuthenticated())
+      next()
+    else
+      res.status = 401
+      res.send('{ Error: "Invalid credentials"}')
+  }
+
   return passport
 }
--- app/server/routes/index.js
+++ app/server/routes/index.js
@@ -1,5 +1,6 @@
 const static = require('express').static 
 const bodyParser = require('body-parser')
+const escape = require('escape-html')
 const setupPassport = require('../config/passport')
 
 const setupAuth = require('./auth')
@@ -18,8 +19,9 @@ module.exports = (app) => {
 
   app.use('/static', static('../public'))
   app.get('/', (req, res) => {
-    res.render('index.pug', {NODE_ENV: process.env.NODE_ENV})
-    console.log(req.body)
-    console.log(req.path)
+    res.render('index.pug', {
+      NODE_ENV: process.env.NODE_ENV, 
+      user: req.user ? JSON.stringify(req.user) : '{}',
+    })
   })
 }
